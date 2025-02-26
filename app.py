024a7670972d78bfe5846469447638704e64e690
EduardodosSantosRocha2 <duduredtone@gmail.com>
2025-02-26 16:09:06 -0300

    feat: API ProtozoAIro

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
-import cv2
+# import cv2
 import numpy as np
 import os
 import base64
@@ -18,39 +18,39 @@ app = Flask(__name__)
 def home():
      return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    try:
-        data = request.json
-        img_data_64 = data.get("image")
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     try:
+#         data = request.json
+#         img_data_64 = data.get("image")
         
-        if not img_data_64:
-            return jsonify({"error": "Nenhuma imagem fornecida"}), 400
+#         if not img_data_64:
+#             return jsonify({"error": "Nenhuma imagem fornecida"}), 400
         
-        img_bytes = base64.b64decode(img_data_64)
-        imagem_teste = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+#         img_bytes = base64.b64decode(img_data_64)
+#         imagem_teste = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
 
-        if imagem_teste is None:
-            return jsonify({"error": "Erro ao decodificar a imagem"}), 400
-
-        
-        image_padronizada = cv2.resize(imagem_teste, (64, 64)) / 255.0 # Redimensionando
-        image_padronizada = image_padronizada.reshape(-1, 64, 64, 3) # Normalizando a imagem
+#         if imagem_teste is None:
+#             return jsonify({"error": "Erro ao decodificar a imagem"}), 400
 
         
-        with open(json_path, 'r', encoding='utf-8') as json_file: # Carregar modelo
-            json_saved_model = json_file.read()
-            network_loaded = tf.keras.models.model_from_json(json_saved_model)
-            network_loaded.load_weights(hdf5)
-            network_loaded.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#         image_padronizada = cv2.resize(imagem_teste, (64, 64)) / 255.0 # Redimensionando
+#         image_padronizada = image_padronizada.reshape(-1, 64, 64, 3) # Normalizando a imagem
+
+        
+#         with open(json_path, 'r', encoding='utf-8') as json_file: # Carregar modelo
+#             json_saved_model = json_file.read()
+#             network_loaded = tf.keras.models.model_from_json(json_saved_model)
+#             network_loaded.load_weights(hdf5)
+#             network_loaded.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     
-        previsao = network_loaded.predict(image_padronizada)
-        result = 'Parasitized' if np.argmax(previsao) == 0 else 'Uninfected'
+#         previsao = network_loaded.predict(image_padronizada)
+#         result = 'Parasitized' if np.argmax(previsao) == 0 else 'Uninfected'
 
-        return jsonify({'predict': result})
+#         return jsonify({'predict': result})
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 
 
